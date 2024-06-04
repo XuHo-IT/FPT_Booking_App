649cdfc055ce878061a7422b6b5c06a4a519c381
PC <PC@10.12.68.75>
2024-06-04 16:03:11 +0700

    gay2

--- src/main/java/Model/Room.java
+++ src/main/java/Model/Room.java
@@ -9,5 +9,64 @@ package Model;
  * @author PC
  */
 public class Room {
+    private String roomId;
+    private String roomtype;
+    private int capacity;
+    private double price;
+    private String status;
+
+    public Room(String roomId, String roomtype, int capacity, double price, String status) {
+        this.roomId = roomId;
+        this.roomtype = roomtype;
+        this.capacity = capacity;
+        this.price = price;
+        this.status = status;
+    }
+
+    public String getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(String roomId) {
+        this.roomId = roomId;
+    }
+
+    public String getRoomtype() {
+        return roomtype;
+    }
+
+    public void setRoomtype(String roomtype) {
+        this.roomtype = roomtype;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Room{" + "roomId=" + roomId + ", roomtype=" + roomtype + ", capacity=" + capacity + ", price=" + price + ", status=" + status + '}';
+    }
+    
     
 }
