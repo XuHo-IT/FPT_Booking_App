64a18c97249d61e371a95468b5064373beee92aa
XUHO <ngotranxuanhoa09062004@gmail.com>
2024-06-04 08:40:52 +0700

    2

--- src/main/java/Model/User.java
+++ src/main/java/Model/User.java
@@ -4,10 +4,105 @@
  */
 package Model;
 
+import java.sql.Date;
+
 /**
  *
  * @author ADMIN
  */
 public class User {
+    private int Id;
+    private String userName;
+    private Date DoB;
+    private String phone;
+    private boolean gender;
+    private String address;
+    private String email;
+    private String password;
+
+    public User(int Id, String userName, Date DoB, String phone, boolean gender, String address, String email, String password) {
+        this.Id = Id;
+        this.userName = userName;
+        this.DoB = DoB;
+        this.phone = phone;
+        this.gender = gender;
+        this.address = address;
+        this.email = email;
+        this.password = password;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int Id) {
+        this.Id = Id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public Date getDoB() {
+        return DoB;
+    }
+
+    public void setDoB(Date DoB) {
+        this.DoB = DoB;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public boolean isGender() {
+        return gender;
+    }
+
+    public void setGender(boolean gender) {
+        this.gender = gender;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "Id=" + Id + ", userName=" + userName + ", DoB=" + DoB + ", phone=" + phone + ", gender=" + gender + ", address=" + address + ", email=" + email + ", password=" + password + '}';
+    }
+
+    public User() {
+    }
+    
+    
     
 }
